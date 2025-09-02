31b9007c8e674bf17a6ce138fd4e845e0e74b8b1
johnlatif16 <johnlatif37@gmail.com>
2025-09-02 15:51:27 +0300

    Update server.js

--- server.js
+++ server.js
@@ -282,4 +282,4 @@ app.delete('/api/requests/:id', async (req, res) => {
 // ==============================================
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));كده؟
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
