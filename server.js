3b7dfdcaf45268c13a40c1fdb9daf9995d4299ec
johnlatif16 <johnlatif37@gmail.com>
2025-09-06 11:20:25 +0300

    Update server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ if (!fs.existsSync(uploadsDir)) fs.mkdirSync(uploadsDir);
 let transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
-  secure: true, // لازم true مع 465
+  secure: false, // مع 587 لازم false
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
